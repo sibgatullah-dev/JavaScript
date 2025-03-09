e57acd2bee5870e5f2853539eaab4de65c84bff4
Sibgatullah <sibgatullah881@gmail.com>
2025-03-09 19:46:54 +0600

    Objects and Arrays

---
+++ Arrays/array-to-string.js
@@ -0,0 +1,4 @@
+//The javascript method toString() converts an array to a string of (separated by commas) arrays values
+const fruits = ["banana","orange","mango","apple"];
+
+console.log(fruits.toString());
