20289c60f3f623c9334354e26084f5c4bc2d7c9c
Sibgatullah <sibgatullah881@gmail.com>
2025-03-09 19:40:01 +0600

    Objects and Arraays

---
+++ Arrays/javaScript_arrays.js
@@ -0,0 +1,9 @@
+const cars = ["Saab","Ford","Volvo","BMW","Toyota"];
+console.log(cars);
+console.log(cars[0]);
+//Index 5 will be undefined cause there is no value for index 5
+console.log(cars[5]);
+
+//Changing an array element 
+cars[1]="Honda";
+console.log(cars[1]);
