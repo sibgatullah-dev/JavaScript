2e4c8c1fc46ad9413e8152e4c7508b544aa4c399
Sibgatullah <sibgatullah881@gmail.com>
2025-03-21 23:22:06 +0600

    adding array element

--- Arrays/addaing_array_element.js
+++ Arrays/addaing_array_element.js
@@ -3,3 +3,8 @@ const fruits = ["Banana", "Orange", "Apple"];
 fruits.push('lemon');
 console.log(fruits);
 console.log(fruits.length);
+
+//Adding elements with high indexes can create undefined "holes" in an array:
+const fruit = ["Banana", "Orange", "Apple"];
+fruit[6]= "Lemon";
+console.log(fruit);//if you turn the array into flat string you will see undefined values as holes!!
