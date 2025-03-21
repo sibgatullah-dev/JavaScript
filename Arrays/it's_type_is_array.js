248916aa556112acf82453a9479309c314e30217
Sibgatullah <sibgatullah881@gmail.com>
2025-03-21 23:47:17 +0600

    type of array

--- Arrays/it's_type_is_array.js
+++ Arrays/it's_type_is_array.js
@@ -3,6 +3,11 @@ console.log(typeof fruits);//The typeof operator returns object because a JavaSc
 
 //there are 2 solution to this 
 //Solution1
-//To solve this problem ECMAScript 5 (JavaScript 2009) defined a new method Array.isArray():
+//To solve this problem ECMAScript 5 (JavaScript 2009) defined a new method Array.isArray()
 const fruit = ["Banana", "Orange", "Apple"];
 console.log(Array.isArray(fruit));//The answer will be true as we asked if its array or not
+
+//Solution2
+//The instanceof operator returns true if an object is created by a given constructor:
+const Fruit = ["Banana", "Orange", "Apple"];
+console.log(Fruit instanceof Array);//The answer will be tru as we are claiming that the type of Fruit is array . If it wasn't an array then the answer would be false
